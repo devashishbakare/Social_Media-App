53b5e46872df4fb603bbe1b59467e6a43c7ce0b2
devashish bakare <devashishbakare@gmail.com>
2022-08-21 13:12:10 +0530

    Added Delete comment functionality

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -21,8 +21,23 @@ module.exports.create = function(req, res){
                 
                 return res.redirect("back");
             });
-        }
-        
+        }        
     });
+}
 
+module.exports.deleteComment = function(req, res){
+    
+    Comment.findById(req.params.id, function(err, comment){
+
+        if(comment.user == req.user.id){
+            let postId = comment.post;
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId, { $pull : {comments : req.params.id}}, function(err, post){
+                return res.redirect("back");
+            });
+        }else{
+            return res.redirect("back");
+        } 
+    });
 }
--- routes/comment.js
+++ routes/comment.js
@@ -7,5 +7,6 @@ const passport = require("passport");
 const commentController = require("../controllers/comments_controller");
 
 router.post("/create", passport.checkAuthentication, commentController.create);
+router.get("/deleteComment/:id", passport.checkAuthentication, commentController.deleteComment);
 
 module.exports = router;
